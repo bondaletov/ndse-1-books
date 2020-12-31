e9b75333254c77b25cc47e27bb6b8afb849cda43
constantine <20419408+bondaletov@users.noreply.github.com>
2020-12-31 15:00:09 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -39,19 +39,19 @@ app.get('/', function (req, res) {
 
 app.post('/api/user/login', function (req, res) {
     res.status(200);
-   res.end(JSON.stringify(MOCK_USER));
+    res.json(MOCK_USER);
 });
 
 app.get('/api/books', function (req, res) {
    res.status(200);
-   res.end(JSON.stringify(stor.books));
+   res.json(stor.books);
 });
 
 app.get('/api/books/:id', function (req, res) {
     const {id} = req.params;
-    const book = stor.books.find(b => b.id == id);
+    const book = stor.books.find(b => b.id == id) || {};
     res.status(200);
-    res.end(JSON.stringify(book || {}));
+    res.json(book);
 });
 
 app.post('/api/books', function (req, res) {
@@ -59,15 +59,15 @@ app.post('/api/books', function (req, res) {
     const book = new Book(undefined, title, description, authors, favorite, fileCover, fileName);
     stor.books.push(book);
     res.status(201);
-    res.end(JSON.stringify(book));
+    res.json(book);
 });
 
 app.put('/api/books/:id', function (req, res) {
     const {id} = req.params;
     const bookIdx = stor.books.findIndex(b => b.id == id);
     if (bookIdx == -1) {
-        res.status(204);
-        res.end();
+        res.status(404);
+        res.end("Not found");
     }
     // TODO add validators for required fields
     const book = {
@@ -76,8 +76,7 @@ app.put('/api/books/:id', function (req, res) {
     };
     stor.books[bookIdx] = book;
     res.status(200);
-    res.end(JSON.stringify(book));
-
+    res.json(book);
 });
 
 app.delete('/api/books/:id', function (req, res) {
